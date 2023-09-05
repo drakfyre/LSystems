c1a9c73486acc15c96eacaba556160857629c463
Maximilian Maroe <drakfyre@gmail.com>
2023-09-04 19:59:27 -0700

    Camera work

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     public Transform target;
+    public float speed = 1.0f;
+    public float rotationSpeed = 60.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        Quaternion lookRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
     }
 }
