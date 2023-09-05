90948bde4ce49a12b837e3c343bb9523af5b75db
Maximilian Maroe <drakfyre@gmail.com>
2023-09-05 13:46:56 -0700

    Perlin height added to TurtleGraphics so it works again with non-instant speeds

--- Assets/TurtleGraphics.cs
+++ Assets/TurtleGraphics.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TurtleGraphics : MonoBehaviour
@@ -8,6 +9,10 @@ public class TurtleGraphics : MonoBehaviour
     public float moveSpeed = 1.0f;
     public float rotationAmount = 90.0f;
     public int iterations = 5;
+
+    public bool usePerlinHeight = false;
+    public float perlinFrequency = 1.0f;
+    public float perlinAmplitude = 1.0f;
 
     public string lString;
     public List<char> ruleCharacters = new List<char>();
@@ -21,19 +26,27 @@ public class TurtleGraphics : MonoBehaviour
         public Quaternion rotation;
     }
     private Stack<State> stateStack = new Stack<State>();
+    private float startingY;
 
     // Start is called before the first frame update
     void Start()
     {
-        lSystem.lSystemString = lString;
+        startingY = transform.position.y;
 
+        if(usePerlinHeight)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.y = Mathf.PerlinNoise(transform.position.x * perlinFrequency, transform.position.z * perlinFrequency) * perlinAmplitude + startingY;
+            transform.position = newPosition;
+        }
+
+
+        lSystem.lSystemString = lString;
         for(int i = 0; i < ruleCharacters.Count; i++)
         {
             lSystem.replacementStrings.Add(ruleCharacters[i],ruleStrings[i]);
         }
-
         lSystem.Iterate(iterations);
-
         Debug.Log(lSystem.lSystemString);
 
         StartCoroutine(Draw());
@@ -49,12 +62,17 @@ public class TurtleGraphics : MonoBehaviour
             {
                 case 'F':
                     Vector3 targetPosition = transform.position + transform.forward * moveDistance;
-                    while(Vector3.Distance(transform.position, targetPosition) > 0.1f)
+                    if(usePerlinHeight)
+                    {
+                        targetPosition.y = Mathf.PerlinNoise(targetPosition.x * perlinFrequency, targetPosition.z * perlinFrequency) * perlinAmplitude + startingY;
+                    }
+                    while (Vector3.Distance(transform.position, targetPosition) > 0.001f)
                     {
                         transform.position = Vector3.MoveTowards(transform.position,targetPosition, moveSpeed * Time.deltaTime);
                         yield return null;
                     }
                     transform.position = targetPosition;
+                    yield return null;
                     break;
                 case '+':
                     transform.Rotate(0.0f,rotationAmount,0.0f);
